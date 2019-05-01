4fe491d62cd35a529e2c3ee93593516567db0fde
ghstahl@gmail.com <ghstahl@gmail.com>
2019-05-01 08:23:25 -0700

    Removed LazyService.cs and NOT using LazyService as it doesn't work with IEnumerable

---
+++ src/IdentityServer4ExtensionGrants.P7Core/Extensions/ApplicationBuilderExtensions.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Rewrite;
+using P7Core.IRules;
+
+namespace P7Core.Extensions
+{
+    public static class ApplicationBuilderExtensions
+    {
+        public static IApplicationBuilder UseLowercaseRewriter(this IApplicationBuilder app)
+        {
+            app.UseRewriter(new RewriteOptions().Add(new RewriteLowerCaseRule()));
+            return app;
+        }
+    }
+}
---
+++ src/IdentityServer4ExtensionGrants.P7Core/Extensions/LazyServiceCollectionExtensions.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace P7Core.Extensions
+{
+    public static class LazyServiceCollectionExtensions
+    {
+        public static IServiceCollection AddLazyTransient<TService, TImplementation>(this IServiceCollection services)
+            where TService : class
+            where TImplementation : class, TService
+        {
+            services.AddTransient<TImplementation>();
+            services.AddTransient<Lazy<TService>>(serviceProvider =>
+            {
+
+                return new Lazy<TService>(() =>
+                {
+                    var impl = serviceProvider.GetRequiredService<TImplementation>();
+                    return impl;
+                });
+            });
+            return services;
+        }
+        public static IServiceCollection AddLazySingleton<TService, TImplementation>(this IServiceCollection services)
+            where TService : class
+            where TImplementation : class, TService
+        {
+            services.AddSingleton<TImplementation>();
+            services.AddSingleton<Lazy<TService>>(serviceProvider =>
+            {
+
+                return new Lazy<TService>(() =>
+                {
+                    var impl = serviceProvider.GetRequiredService<TImplementation>();
+                    return impl;
+                });
+            });
+            return services;
+        }
+        public static IServiceCollection AddLazyScoped<TService, TImplementation>(this IServiceCollection services)
+            where TService : class
+            where TImplementation : class, TService
+        {
+            services.AddScoped<TImplementation>();
+            services.AddScoped<Lazy<TService>>(serviceProvider =>
+            {
+
+                return new Lazy<TService>(() =>
+                {
+                    var impl = serviceProvider.GetRequiredService<TImplementation>();
+                    return impl;
+                });
+            });
+            return services;
+        }
+
+    }
+}
