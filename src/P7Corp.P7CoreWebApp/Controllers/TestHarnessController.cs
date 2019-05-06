ec05b8882967fb9d36c29e00f42a7697e704a23f
ghstahl@gmail.com <ghstahl@gmail.com>
2019-05-06 10:37:09 -0700

    missing

---
+++ src/P7Corp.P7CoreWebApp/Controllers/TestHarnessController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using IdentityModelExtras;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace P7Corp.P7CoreWebApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestHarnessController : ControllerBase
+    {
+        private IDefaultHttpClientFactory _defaultHttpClientFactory;
+        private HttpClient _httpClient;
+        private HttpMessageHandler _httpMessageHandler;
+
+        public TestHarnessController(IDefaultHttpClientFactory defaultHttpClientFactory)
+        {
+            _defaultHttpClientFactory = defaultHttpClientFactory;
+            _httpClient = _defaultHttpClientFactory.HttpClient;
+            _httpMessageHandler = _defaultHttpClientFactory.HttpMessageHandler;
+        }
+
+
+        // GET: api/TestHarness
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+    }
+}
