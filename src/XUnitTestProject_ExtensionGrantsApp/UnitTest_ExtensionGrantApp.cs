f6b98c90f9058cc9811d3b9c3e72a0d4ed5b5cfd
ghstahl@gmail.com <ghstahl@gmail.com>
2019-05-07 15:57:04 -0700

    removed unnessary tests

--- src/XUnitTestProject_ExtensionGrantsApp/UnitTest_ExtensionGrantApp.cs
+++ src/XUnitTestProject_ExtensionGrantsApp/UnitTest_ExtensionGrantApp.cs
@@ -32,55 +32,5 @@ namespace XUnitTestProject_ExtensionGrantsApp
 
         }
 
-        [Fact]
-        public async Task Test_arbitrary_resource_owner_success()
-        {
-            var client = _fixture.Client;
-            var req = new HttpRequestMessage(HttpMethod.Post, "/api/auth/arbitrary_resource_owner")
-            {
-                // Content = new FormUrlEncodedContent(dict)
-            };
-            var response = await client.SendAsync(req);
-            response.StatusCode.ShouldBe(System.Net.HttpStatusCode.OK);
-            var jsonString = await response.Content.ReadAsStringAsync();
-            jsonString.ShouldNotBeNullOrWhiteSpace();
-
-            var values = JsonConvert.DeserializeObject<SafeTokenRawResult>(jsonString);
-            values.ShouldNotBeNull();
-
-
-        }
-        public class TokenErrorRawResult
-        {
-            public TokenErrorResult TokenErrorResult { get; set; }
-
-        }
-
-        [Fact]
-        public async Task Test_arbitrary_resource_owner_bad_client_success()
-        {
-            var client = _fixture.Client;
-            var req = new HttpRequestMessage(HttpMethod.Post, "/api/auth/arbitrary_resource_owner_bad_client")
-            {
-                // Content = new FormUrlEncodedContent(dict)
-            };
-            var response = await client.SendAsync(req);
-            response.StatusCode.ShouldBe(System.Net.HttpStatusCode.OK);
-            var jsonString = await response.Content.ReadAsStringAsync();
-            jsonString.ShouldNotBeNullOrWhiteSpace();
-
-            var settings = new JsonSerializerSettings
-            {
-                NullValueHandling = NullValueHandling.Ignore,
-                MissingMemberHandling = MissingMemberHandling.Ignore
-            };
-
-            var value = JsonConvert.DeserializeObject<SafeTokenRawResult>(jsonString, settings);
-            value.ShouldNotBeNull();
-            value.TokenErrorResult.ShouldNotBeNull();
-
-
-        }
-
     }
 }
