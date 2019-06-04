7c75ab14a4dd004d993e9cc8b67bc7c7d18a2bb3
Herb Stahl <ghstahl@gmail.com>
2019-06-04 09:55:19 -0700

    rollup build

---
+++ build.bat
@@ -0,0 +1,5 @@
+dotnet clean ./src/IdentityServer4-Extension-Grants.sln
+dotnet restore ./src/IdentityServer4-Extension-Grants.sln
+dotnet build ./src/IdentityServer4-Extension-Grants.sln
+dotnet test ./src/IdentityServer4-Extension-Grants.sln --no-build
+dotnet pack ./src/IdentityServer4-Extension-Grants.sln --include-source --include-symbols /property:Version=1.2.3.4
