4b320d5fac1a47abfa5d179b8a80eca576a10e22
Herb Stahl <ghstahl@gmail.com>
2019-06-05 06:47:52 -0700

    working Cosmos DB operation store

--- build.bat
+++ build.bat
@@ -2,4 +2,4 @@ dotnet clean ./src/IdentityServer4-Extension-Grants.sln
 dotnet restore ./src/IdentityServer4-Extension-Grants.sln
 dotnet build ./src/IdentityServer4-Extension-Grants.sln
 dotnet test ./src/IdentityServer4-Extension-Grants.sln --no-build
-dotnet pack ./src/IdentityServer4-Extension-Grants.sln --include-source --include-symbols /property:Version=1.2.3.4
+dotnet pack ./src/IdentityServer4-Extension-Grants.sln --include-source --include-symbols /property:Version=1.0.0-beta-10
