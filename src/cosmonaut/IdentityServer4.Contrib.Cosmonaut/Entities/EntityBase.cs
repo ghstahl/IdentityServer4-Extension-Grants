b25f032275187bf2136cce89c4087da68be2386d
Herb Stahl <ghstahl@gmail.com>
2019-06-10 07:33:34 -0700

    refactor missing

---
+++ src/cosmonaut/IdentityServer4.Contrib.Cosmonaut/Entities/EntityBase.cs
@@ -0,0 +1,13 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IdentityServer4.Contrib.Cosmonaut.Entities
+{
+    public class EntityBase
+    {
+        [JsonProperty("id")]
+        public string Id { get; set; }
+    }
+}
---
+++ src/cosmonaut/IdentityServer4.Contrib.Cosmonaut/Entities/IdentityResourceEntity.cs
@@ -0,0 +1,15 @@
+﻿using Newtonsoft.Json;
+
+namespace IdentityServer4.Contrib.Cosmonaut.Entities
+{
+    public class IdentityResourceEntity: ResourceEntity
+    {
+        [JsonProperty("required")]
+        public bool Required { get; set; }
+        [JsonProperty("emphasize")]
+        public bool Emphasize { get; set; }
+
+        [JsonProperty("showInDiscoveryDocument")]
+        public bool ShowInDiscoveryDocument { get; set; }
+    }
+}
