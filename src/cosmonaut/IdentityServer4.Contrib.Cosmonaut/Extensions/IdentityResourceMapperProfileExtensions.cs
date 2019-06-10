9cca5c8577d273657aa2da5245c47541648a4fe0
Herb Stahl <ghstahl@gmail.com>
2019-06-10 07:35:06 -0700

    missing

---
+++ src/cosmonaut/IdentityServer4.Contrib.Cosmonaut/Extensions/IdentityResourceMapperProfileExtensions.cs
@@ -0,0 +1,37 @@
+﻿using AutoMapper;
+using IdentityServer4.Contrib.Cosmonaut.Mappers;
+using IdentityServer4.Contrib.Cosmonaut.Models;
+using IdentityServer4.Models;
+
+namespace IdentityServer4.Contrib.Cosmonaut.Extensions
+{
+    /// <summary>
+    ///     Extension methods to map ApiResource to
+    ///     ApiResourceEntity.
+    /// </summary>
+    public static class IdentityResourceMapperProfileExtensions
+    {
+        static IdentityResourceMapperProfileExtensions()
+        {
+            Mapper = new MapperConfiguration(cfg => cfg.AddProfile<IdentityResourceMapperProfile>())
+                .CreateMapper();
+        }
+
+        internal static IMapper Mapper { get; }
+
+        public static IdentityResource ToModel(this Entities.IdentityResourceEntity entity)
+        {
+            return entity == null ? null : Mapper.Map<IdentityResource>(entity);
+        }
+
+        public static Entities.IdentityResourceEntity ToEntity(this IdentityResource model)
+        {
+            return model == null ? null : Mapper.Map<Entities.IdentityResourceEntity>(model);
+        }
+
+        public static void UpdateEntity(this IdentityResource model, Entities.IdentityResourceEntity entity)
+        {
+            Mapper.Map(model, entity);
+        }
+    }
+}
---
+++ src/cosmonaut/IdentityServer4.Contrib.Cosmonaut/Mappers/IdentityResourceMapper.cs
@@ -0,0 +1,32 @@
+﻿using AutoMapper;
+using IdentityServer4.Contrib.Cosmonaut.Entities;
+using IdentityServer4.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IdentityServer4.Contrib.Cosmonaut.Mappers
+{
+    /// <inheritdoc />
+    /// <summary>
+    ///     AutoMapper Config for ApiResource
+    ///     Between Model and Entity
+    ///     <seealso cref="!:https://github.com/AutoMapper/AutoMapper/wiki/Configuration">
+    ///     </seealso>
+    /// </summary>
+    public class IdentityResourceMapperProfile : Profile
+    {
+        /// <summary>
+        ///     <see cref="IdentityResourceMapperProfile">
+        ///     </see>
+        /// </summary>
+        public IdentityResourceMapperProfile()
+        {
+            // entity to model
+            CreateMap<IdentityResourceEntity, IdentityResource>(MemberList.Destination);
+
+            // model to entity
+            CreateMap<IdentityResource, IdentityResourceEntity>(MemberList.Source);
+        }
+    }
+}
